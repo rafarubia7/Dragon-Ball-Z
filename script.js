b33746da04fe17a4592a43611e21f3fa59965f31
Rafael Rubiá Oliveira Cardoso <166622515+rafarubia7@users.noreply.github.com>
2024-10-18 10:47:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+        closeModal(event.target.id);
+    }
+}
